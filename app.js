7c8fb521363d6fbffaf60ab4fedf4bb758eb2b81
ismayil61 <ismayilsahane@gmail.com>
2025-12-06 11:49:50 +0400

    ders19-js

---
+++ app.js
@@ -0,0 +1,13 @@
+function time() {
+    let tarix = new Date();
+
+    let spanSaat = document.getElementById("saat");
+    let spanDeq = document.getElementById("deq");
+    let spanSan = document.getElementById("san");
+
+    spanSaat.innerText = tarix.getHours().toString().padStart(2, "0");
+    spanDeq.innerText = tarix.getMinutes().toString().padStart(2, "0");
+    spanSan.innerText = tarix.getSeconds().toString().padStart(2, "0");
+}
+
+setInterval(time, 1000);
